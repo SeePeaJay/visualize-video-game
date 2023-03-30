6f880c79c3fdab5ad8c4418693a915081e77df34
SeePeaJay <cpj99@proton.me>
2023-03-29 23:29:33 -0700

    feat: pie chart legend

--- js/pieChart.js
+++ js/pieChart.js
@@ -7,6 +7,9 @@ class PieChart {
       margin: {
         top: 10, right: 110, bottom: 10, left: 10,
       },
+      legendCategoryWidth: 80,
+      legendCategoryHeight: 14,
+      legendCategoryRadius: 5,
     };
     this.dispatcher = _dispatcher;
     this.data = _data;
@@ -18,33 +21,36 @@ class PieChart {
   initVis() {
     const vis = this;
 
-    /* Define inner chart dimensions */
+    /* define inner chart dimensions */
     vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
     vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
     vis.radius = Math.min(vis.width, vis.height) / 2;
 
-    /* Define size of SVG drawing area */
+    /* define size of SVG drawing area */
     vis.svg = d3.select('#pie-vis').append('svg')
       .attr('id', 'pie-chart')
       .attr('width', vis.config.containerWidth)
       .attr('height', vis.config.containerHeight);
 
-    /* Append chartArea (the group element that will contain the actual arcs)
+    /* append chartArea (the group element that will contain the actual arcs)
     and position it according to the given margin config */
     vis.chartArea = vis.svg.append('g')
       .attr('transform', `translate(${vis.radius + vis.config.margin.left},${vis.radius + vis.config.margin.top})`);
 
-    /* Set up the colors for the chart */
+    // initialize legend group
+    vis.legend = vis.svg.append('g');
+
+    /* set up the colors for the chart */
     vis.color = d3.scaleOrdinal()
       .domain(Object.keys(vis.data))
       .range(d3.schemeSet2);
 
-    /* Set up the arc generator */
+    /* set up the arc generator */
     vis.arc = d3.arc()
       .outerRadius(vis.radius - 10)
       .innerRadius(0);
 
-    /* Set up the pie generator */
+    /* set up the pie generator */
     vis.pie = d3.pie()
       .sort(null)
       .value((d) => d.value);
@@ -58,32 +64,55 @@ class PieChart {
     vis.entries = Object.keys(vis.data).map((key) => ({ key, value: vis.data[key] }));
 
     vis.renderVis();
-    // vis.renderLegend();
+    vis.renderLegend();
   }
 
-  /* Bind data to visual elements */
+  /* bind data to visual elements */
   renderVis() {
     const vis = this;
 
-    /* Draw the slices of the pie chart */
+    /* draw the slices of the pie chart */
     vis.arcs = vis.chartArea.selectAll('.arc')
       .data(vis.pie(vis.entries))
       .enter() // enter should be fine ...
       .append('g')
       .attr('class', 'arc');
 
-    // Draw each slice of the pie chart
+    /* draw each slice of the pie chart */
     vis.arcs.append('path')
       .attr('d', vis.arc) // must be able to access startAngle and endAngle somehow
       .attr('fill', (d) => vis.color(d.data.key)); // d.data due to pie it seems
-
-    // Add labels to the slices
-    vis.arcs.append('text')
-      .attr('transform', (d) => `translate(${vis.arc.centroid(d)})`)
-      .attr('text-anchor', 'middle')
-      .text((d) => d.data.key);
   }
 
-  // renderLegend() {
-  // }
+  renderLegend() {
+    const vis = this;
+
+    // the function for placing legend categories
+    const getTranslate = (d, i) => {
+      const tx = 200; const ty = 120;
+      const x = (i % 1) * vis.config.legendCategoryWidth + tx;
+      const y = i * vis.config.legendCategoryHeight + ty;
+
+      return `translate(${x}, ${y})`;
+    };
+
+    vis.legendElements = vis.legend.selectAll('g')
+      .data(Object.keys(vis.data))
+      .join('g')
+      .attr('transform', getTranslate);
+
+    /* render the icons */
+    vis.legendElements.selectAll('circle')
+      .data((d) => [d])
+      .join('circle')
+      .attr('r', vis.config.legendCategoryRadius)
+      .attr('fill', (d) => vis.color(d));
+
+    /* render the texts */
+    vis.legendElements.selectAll('text')
+      .data((d) => [d])
+      .join('text')
+      .attr('transform', `translate(${vis.config.legendCategoryRadius * 2}, ${vis.config.legendCategoryRadius})`)
+      .text((d) => d);
+  }
 }
